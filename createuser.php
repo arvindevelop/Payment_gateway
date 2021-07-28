0b0347bf6acf5f9a7ff3e914984d52a4cab598f5
Arvind Kumar Singh <73786871+arvindevelop@users.noreply.github.com>
2021-07-29 00:17:59 +0530

    Update createuser.php
    Done

--- createuser.php
+++ createuser.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create User</title>
-    <link rel="stylesheet" type="text/css" href="css/create.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" 
-    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" 
+    integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
+    crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/navbar.css">
-    
+    <link rel="stylesheet" type="text/css" href="css/create.css">
+    <title>Create User</title>
 </head>
 
 <body style="background-color : #fab1a0;">
@@ -30,22 +31,28 @@
   ?>
 
   <!-- navbar --> 
-  <nav class="navbar navbar-expand-md " style="background-color : grey;">
-        <a class="navbar-brand" href="index.php" style="color : #FFCD00;font-weight:bold;font-size:largest"><b>MY BANK</b></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-          <span class="navbar-toggler-icon"></span>
+  <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><b style="color:yellow;">MY BANK</b></a>
+        <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="collapsibleNavbar">
-              <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                  <a class="nav-link" href="index.php" style="color : white;"><b>Back to Home</b></a>
-                </li>
-            </div>
-        </nav>
+        <div class="collapse navbar-collapse" id="navbarContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">  </ul>
+            <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : white;"><b>Back to Home</b></a>
+              </li>
+             
+            </ul>
+        </div>
+    </div>
+</nav>
   <!--navbar end-->
+
+  <!--main content-->
   <h2 class="text-center pt-4" style="color : indigo;">Create Account</h2>
   <br>
-
   <div class="background1">
     <div class="container1">
       <div class="screen">
@@ -67,8 +74,8 @@
                 <input class="app-form-control" placeholder="PASSWORD" type="password" name="password" required>
               </div><br>
               <div class="app-form-group float-right">
-                <input class="app-form-button btn btn-info" type="submit" value="CREATE" name="submit"></input>
-                <input class="app-form-button btn btn-info" type="reset" value="RESET" name="reset"></input>
+                <input class="app-form-button btn" style="background-color : #00cec9;" type="submit" value="CREATE" name="submit"></input>
+                <input class="app-form-button btn" style="background-color : #00cec9;" type="reset" value="RESET" name="reset"></input>
               </div>
             </form>
           </div>
@@ -76,10 +83,10 @@
       </div>
     </div>
   </div><br>
+
+  
   <!--footer-->
   <?php include 'footer.php';?>
   <!--footer end-->
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </body>
 </html>
