4413d89614a6928f19f81ab36c2093fc01288ade
Arvind Kumar Singh <73786871+arvindevelop@users.noreply.github.com>
2021-07-29 00:24:07 +0530

    Update my_bank.sql
    Done

--- sql/my_bank.sql
+++ sql/my_bank.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 09, 2021 at 01:35 PM
+-- Generation Time: Jul 28, 2021 at 08:40 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.0
 
@@ -29,6 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `transaction` (
   `sno` int(3) NOT NULL,
+  `id` int(5) DEFAULT NULL,
   `sender` text NOT NULL,
   `receiver` text NOT NULL,
   `balance` int(8) NOT NULL,
@@ -39,13 +40,15 @@ CREATE TABLE `transaction` (
 -- Dumping data for table `transaction`
 --
 
-INSERT INTO `transaction` (`sno`, `sender`, `receiver`, `balance`, `datetime`) VALUES
-(1, 'Shivam jaiswal', 'sonu kumar', 500, '2021-05-18 14:17:59'),
-(2, 'Chandan Maurya', 'rahul kumar', 1000, '2021-05-18 14:18:22'),
-(3, 'harshit kumar', 'Laxman Kumar', 500, '2021-05-18 16:16:46'),
-(4, 'rahul kumar', 'sonu kumar', 500, '2021-05-18 19:23:01'),
-(5, 'Shivam jaiswal', 'Laxman Kumar', 1000, '2021-05-25 17:53:03'),
-(6, 'Arvind Kumar Singh', 'Chandan Maurya', 1000, '2021-06-17 19:35:19');
+INSERT INTO `transaction` (`sno`, `id`, `sender`, `receiver`, `balance`, `datetime`) VALUES
+(1, 3, 'Shivam jaiswal', 'harshit kumar', 100, '2021-07-23 01:19:02'),
+(2, 3, 'Shivam jaiswal', 'Arvind Kumar Singh', 500, '2021-07-23 01:16:48'),
+(16, 4, 'rahul kumar', 'sonu kumar', 100, '2021-07-23 01:21:07'),
+(17, 1, 'Arvind Kumar Singh', 'sonu kumar', 100, '2021-07-28 23:50:45'),
+(18, 2, 'sonu kumar', 'Mahi Kumar', 100, '2021-07-28 23:56:52'),
+(19, 5, 'harshit kumar', 'Anurag Kumar Pandey', 100, '2021-07-28 23:57:48'),
+(20, 6, 'Chandan Maurya', 'Anshu Kumari', 400, '2021-07-28 23:58:32'),
+(21, 7, 'Anshu Kumari', 'Rinku Kumari', 100, '2021-07-28 23:59:25');
 
 -- --------------------------------------------------------
 
@@ -57,25 +60,27 @@ CREATE TABLE `users` (
   `id` int(3) NOT NULL,
   `name` text NOT NULL,
   `email` varchar(30) NOT NULL,
-  `balance` int(8) NOT NULL
+  `balance` int(8) NOT NULL,
+  `password` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `balance`) VALUES
-(1, 'Arvind Kumar Singh', 'b190125@nitsikkim.ac.in', 1000),
-(2, 'sonu kumar', 'b190038@nitsikkim.ac.in', 1500),
-(3, 'Shivam jaiswal', 'b190034@nitsikkim.ac.in', 3000),
-(4, 'rahul kumar', 'b190126@gmail.com', 1000),
-(5, 'harshit kumar', 'b190132@nitsikkim.ac.in', 500),
-(6, 'Chandan Maurya', 'b190129@gmail.com', 19000),
-(7, 'Anshu Kumari', 'anshu@gmail.com', 1500),
-(8, 'Laxman Kumar', 'laxman123@gmail.com', 3500),
-(9, 'Rinku Kumari', 'rinkukr@gmail.com', 1000),
-(10, 'Anurag Kumar Pandey', 'anurag@gmail.com', 5000),
-(11, 'Mahi Kumar', 'mahi@gmail.com', 1500);
+INSERT INTO `users` (`id`, `name`, `email`, `balance`, `password`) VALUES
+(1, 'Arvind Kumar Singh', 'b190125@nitsikkim.ac.in', 1100, 'arvi123'),
+(2, 'sonu kumar', 'b190038@nitsikkim.ac.in', 2500, 'sonu#01'),
+(3, 'Shivam jaiswal', 'b190034@nitsikkim.ac.in', 1400, 'shiv90'),
+(4, 'rahul kumar', 'b190126@gmail.com', 900, '9012345@#'),
+(5, 'harshit kumar', 'b190132@nitsikkim.ac.in', 500, 'harsh786'),
+(6, 'Chandan Maurya', 'b190129@gmail.com', 18600, 'chandu321'),
+(7, 'Anshu Kumari', 'anshu@gmail.com', 2000, 'ansh#23'),
+(8, 'Laxman Kumar', 'laxman123@gmail.com', 3600, 'Lakshy23#23'),
+(9, 'Rinku Kumari', 'rinkukr@gmail.com', 1100, '123456'),
+(10, 'Anurag Kumar Pandey', 'anurag@gmail.com', 5200, 'anuragpandey'),
+(11, 'Mahi Kumar', 'mahi@gmail.com', 2100, 'captaincool'),
+(12, 'Admin', 'adminmybank@mybank.com', 1000000, 'mybankadmin#');
 
 --
 -- Indexes for dumped tables
@@ -101,13 +106,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `transaction`
 --
 ALTER TABLE `transaction`
-  MODIFY `sno` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `sno` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
