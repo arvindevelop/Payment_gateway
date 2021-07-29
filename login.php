19d05b04a5ffbaf57c3e9a0f5db279a5dafc91c5
Arvind Kumar Singh <73786871+arvindevelop@users.noreply.github.com>
2021-07-29 10:10:56 +0530

    Update login.php

--- login.php
+++ login.php
@@ -67,9 +67,11 @@
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">  </ul>
             <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link" href="index.php" style="color : white;"><b>Back to Home</b></a>
+                <a class="nav-link" href="index.php" style="color : white;"><b>Home</b></a>
               </li>
-             
+             <li class="nav-item">
+                <a class="nav-link" href="createuser.php" style="color : white;"><b>Create Account</b></a>
+              </li>
             </ul>
         </div>
     </div>
