3e1ede9d5fefd169a606e7c82e454f4b7b00eb44
Arvind Kumar Singh <73786871+arvindevelop@users.noreply.github.com>
2021-09-17 20:21:16 +0530

    Update config.php
    Connection error message update

--- config.php
+++ config.php
@@ -14,7 +14,7 @@
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
 
 	if(!$conn){
-		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
+		die("Could not connect to the Remote MySQL database due to the following error --> ".mysqli_connect_error());
 	}
 
 ?>
